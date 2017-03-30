36c7857d5e6414108508c46d44d7ec7d811a6316
Arjan Duijzer <arjan@nxtstep.nl>
2017-03-30 17:24:39 +0200

    Switch RxSwift dependency to nxtstep/RxSwift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
         .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", majorVersion: 1),
-        .Package(url: "https://github.com/ReactiveX/RxSwift", majorVersion: 3),
+        .Package(url: "https://github.com/nxtstep/RxSwift", "3.3.2-beta-1),
         .Package(url: "https://github.com/antitypical/Result", majorVersion: 3)
     ],
     exclude: [
